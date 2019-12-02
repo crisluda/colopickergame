030e9032edb8cbe474b414ba69ea53ba5aff005d
crisluda <crisluda2244@gmail.com>
2019-12-02 13:26:14 +0000

    add the logic to the hard and easy btn

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 // var colors=["red","blue","pink","yellow","black","brown"]
-var colors = generateRandomColor(5)
+var numSquares = 6
+var colors = generateRandomColor(numSquares)
 var pikpik = document.querySelector("body")
 var squares = document.querySelectorAll(".square")
 var pickColor = pickColors()
@@ -13,10 +14,32 @@ var hardBtn = document.querySelector("#hardBtn")
 easyBtn.addEventListener("click", function () {
     easyBtn.classList.add("selected")
     hardBtn.classList.remove("selected")
+    var numSquares = 3
+    colors = generateRandomColor(numSquares)
+    pickColor = pickColors()
+    colordisplays.textContent = pickColor
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.background = colors[i]
+        } else {
+            squares[i].style.display = "none"
+        }
+
+
+    }
 })
 hardBtn.addEventListener("click", function () {
     hardBtn.classList.add("selected")
     easyBtn.classList.remove("selected")
+    var numSquares = 6
+    colors = generateRandomColor(numSquares)
+    pickColor = pickColors()
+    colordisplays.textContent = pickColor
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.display = "block"
+
+
+    }
 })
 resetButton.addEventListener("click", function () {
     colors = generateRandomColor(6)
